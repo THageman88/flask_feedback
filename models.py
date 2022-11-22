ace9d4d8584cf916fecacb86abb687abe08a57dd
root <root@LAPTOP-EEV6NRCI.localdomain>
2022-11-22 17:25:12 -0600

    secondary

--- models.py
+++ models.py
@@ -10,5 +10,9 @@ class User(db.Model):
     
     username = db.column(dbtext , nullable = False, unique = True, primary_key = True) 
     password = db.column (db.Text , nullable = False)
-    email = db.column(db.Text , nullable = False)
+    email = db.column(db.String(50), nullable = False)
+    first_name = db.column(db.String(30), nullable = False)
+    last_name = db.column(db.String(30), nullable = False)
+    
+    
     
